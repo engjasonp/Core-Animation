b06b3e97712f76a681fb8609ae867e08b5dbbc79
engjasonp <engjasonp@gmail.com>
2015-09-02 18:00:50 -0700

    Added more options to UIView.animateWithDuration - usingSpringWithDamping and initialSpringVelocity. Added more cases to display different animations.

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -17,11 +17,33 @@ class ViewController: UIViewController {
     @IBAction func tapped(sender: AnyObject) {
         tap.hidden = true
         
-        UIView.animateWithDuration(1, delay: 0, options: .allZeros,
-            animations: { [unowned self] in
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .allZeros, animations: { [unowned self] in
                 switch self.currentAnimation {
                 case 0:
-                    break
+                    self.imageView.transform = CGAffineTransformMakeScale(2, 2)
+                    
+                case 1:
+                    self.imageView.transform = CGAffineTransformIdentity
+                    
+                case 2:
+                    self.imageView.transform = CGAffineTransformMakeTranslation(-256, -256)
+                    
+                case 3:
+                    self.imageView.transform = CGAffineTransformIdentity
+                    
+                case 4:
+                    self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                    
+                case 5:
+                    self.imageView.transform = CGAffineTransformIdentity
+                    
+                case 6:
+                    self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = UIColor.greenColor()
+                    
+                case 7:
+                    self.imageView.alpha = 1
+                    self.imageView.backgroundColor = UIColor.clearColor()
                     
                 default:
                     break
